babd15b49bdccb7019312b2473040e6743fe5d4a
Vadym Rebrov <vadym.rebrov@gmail.com>
2026-01-14 16:47:15 +0200

    Update message attributes

--- src/main/java/dev/profitsoft/internship/rebrov/block_five/model/Message.java
+++ src/main/java/dev/profitsoft/internship/rebrov/block_five/model/Message.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -13,6 +14,10 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.LinkedList;
 import java.util.List;
 
 @Getter
@@ -43,12 +48,22 @@ public class Message {
     @NotBlank(message = "Content cannot be empty")
     private String content;
 
-//  @Field(type = FieldType.Nested)
-//  private List<Attachment> attachments;
+    @Field(type = FieldType.Nested)
+    private List<StatusHistory> history = new LinkedList<>();
 
     @Field(type = FieldType.Keyword)
-    private MessageStatus status;
+    private MessageStatus currentStatus;
 
     @Field(type = FieldType.Text)
-    private String errorMessage;
+    private String currentErrorMessage;
+
+    @Field(type = FieldType.Integer)
+    @Builder.Default
+    private Integer sentAttempt = 0;
+
+    public void addStatus(MessageStatus newStatus, String details) {
+        this.currentStatus = newStatus;
+        this.currentErrorMessage = details;
+        this.history.add(new StatusHistory(newStatus, Instant.now(), details));
+    }
 }
---
+++ src/main/java/dev/profitsoft/internship/rebrov/block_five/model/StatusHistory.java
@@ -0,0 +1,25 @@
+package dev.profitsoft.internship.rebrov.block_five.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusHistory {
+
+    @Field(type = FieldType.Keyword)
+    private MessageStatus status;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis)
+    private Instant timestamp;
+
+    @Field(type = FieldType.Text)
+    private String details;
+}
